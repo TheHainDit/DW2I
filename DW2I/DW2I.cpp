c740566d8dc047485bae239b30c74d1be2a4ab7a
TheHainDit <31826584+TheHainDit@users.noreply.github.com>
2019-06-24 20:11:18 +0300

    Add files via upload

---
+++ DW2I/DW2I.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include "mem.h"
+
+using namespace std;
+
+DWORD WINAPI HackThread(HMODULE hModule) {
+	AllocConsole();
+	FILE* f;
+	freopen_s(&f, "CONOUT$", "w", stdout);
+
+	cout << "DWE | https://steamcommunity.com/id/thehaindit/" << endl;
+	system("pause");
+
+	fclose(f);
+	FreeConsole();
+	FreeLibraryAndExitThread(hModule, 0);
+	return 0;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD fdwReason, LPVOID lpvReserved)
+{
+	switch (fdwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		CloseHandle(CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)HackThread, hModule, 0, nullptr));
+	case DLL_PROCESS_DETACH:
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+		break;
+	}
+	return TRUE;
+}
